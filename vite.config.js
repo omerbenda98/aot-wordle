2bc982fa8a76ab91ac82514f832be60acf3390b1
omerbenda98 <omerbenda98@gmail.com>
2025-10-19 11:39:16 +0300

    dist deleted, docs created

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/aot-wordle/",
+  build: {
+    outDir: "docs", // Change output from 'dist' to 'docs'
+  },
   plugins: [react()],
 });
